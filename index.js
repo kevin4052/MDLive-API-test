d88ccc9dd34e610c134ec279ff4caffeea5e36b0
kevin4052 <kevin.hernandez001@gmail.com>
2021-01-21 11:14:26 -0500

    initial commit for express server

---
+++ index.js
@@ -0,0 +1,26 @@
+// ============ File Imports ==================
+require('dotenv').config();
+
+const express = require("express");
+const bodyParser = require("body-parser");
+// const path = require("path");
+
+const app = express();
+
+// ====== require database configuration ======
+require('./configs/db.config');
+
+// ============= Middleware Setup =============
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// ============== GLOBAL VARIABLES ==================
+
+// default value for title local
+app.locals.title = "Express Routes Example";
+
+
+// ===================== ROUTES =====================
+app.use("/api", require("./routes/index.routes"));
+
+module.exports = app;
---
+++ routes/index.routes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page */
+router.get('/', (req, res) => res.status(200).json({ message: 'API root.' }));
+
+module.exports = router;
