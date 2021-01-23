ede8543f9d37520137b5142be496d1a9f5f9ada7
Kevin Hernandez <kevin.hernandez001@gmail.com>
2021-01-23 16:31:42 -0500

    added 'dotenv' to db.config

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 mongoose
-  .connect(process.env.MONGODB_URI || 'mongodb://localhost/mdlive-apps-api', {
+  .connect(process.env.MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
